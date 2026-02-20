f361ff092b6ead261f87d75607e403665ac3f1b2
hannahrajan <hannah.rajan@gmail.com>
2026-02-20 16:44:31 -0500

    Fixed Character Length, Updated Check Constraints
    Hannah's final push for Phase II A

--- CS4400 Phase II A Template.sql
+++ CS4400 Phase II A Template.sql
@@ -31,27 +31,28 @@ works for you (and runs successfully). */
 
 drop table if exists content;
 create table content (
-ContentID varchar(20) not null,
+ContentID varchar(50) not null,
 Title varchar(50) not null,
 Length int not null,
 Maturity enum('Explicit', 'Not Explicit') not null,
 Release_Date date not null,
-Language varchar(15) not null,
-primary key(ContentID)
+Language varchar(50) not null,
+primary key(ContentID),
+check(Length >= 600)
 );
 
 drop table if exists podcast_series;
 create table podcast_series (
 PodcastID char(7) not null,
 Title varchar(50) not null,
-Description varchar(300) not null,
+Description varchar(100),
 primary key(PodcastID)
 );
 
 drop table if exists podcast_episode;
 create table podcast_episode (
-ContentID varchar(20) not null,
-Topic varchar(20) not null,
+ContentID varchar(50) not null,
+Topic varchar(50) not null,
 Episode_Number int not null,
 PodcastID char(7) not null,
 primary key(ContentID),
@@ -89,18 +90,19 @@ on update restrict on delete cascade
 drop table if exists listener;
 create table listener (
 AccountID char(6) not null,
-Name varchar(30) not null,
+Name varchar(50) not null,
 BDate date not null,
 Email varchar(50) not null,
-Username varchar(30) not null,
+Username varchar(50) not null unique,
 Enrolled_Subscription char(5),
 Timestamp timestamp,
-Stream_ContentID varchar(20),
+Stream_ContentID varchar(50),
 primary key(AccountID),
 foreign key(Enrolled_Subscription) references subscription(SubscriptionID)
 on update restrict on delete set null,
 foreign key(Stream_ContentID) references content(ContentID)
-on update restrict on delete set null
+on update restrict on delete set null,
+check (BDate > '2026-01-01')
 );
 
 drop table if exists friends;
@@ -116,21 +118,22 @@ on update restrict on delete cascade
 drop table if exists creator;
 create table creator (
 AccountID char(6) not null,
-Name varchar(30) not null,
+Name varchar(50) not null,
 BDate date not null,
 Email varchar(50) not null,
-Stage_Name varchar(30) not null,
+Stage_Name varchar(50),
 Biography varchar(100),
-Pinned_ContentID varchar(20),
+Pinned_ContentID varchar(50),
 primary key(AccountID),
 foreign key(Pinned_ContentID) references content(ContentID)
-on update restrict on delete set null
+on update restrict on delete set null,
+check (BDate > '2026-01-01')
 );
 
 drop table if exists creates;
 create table creates (
 CreatorID char(6) not null,
-ContentID varchar(20) not null,
+ContentID varchar(50) not null,
 foreign key(CreatorID) references creator(AccountID)
 on update restrict on delete cascade,
 foreign key(ContentID) references content(ContentID)
@@ -139,7 +142,7 @@ on update restrict on delete cascade
 
 drop table if exists album;
 create table album (
-Name varchar(30) not null,
+Name varchar(50) not null,
 CreatorID char(6) not null,
 primary key(Name, CreatorID),
 foreign key(CreatorID) references creator(AccountID)
@@ -149,16 +152,16 @@ on update restrict on delete cascade
 drop table if exists socials;
 create table socials (
 CreatorID char(6) not null,
-Handle varchar(30) not null,
-Platform varchar(20) not null,
+Handle varchar(50) not null,
+Platform varchar(50) not null,
 foreign key(CreatorID) references creator(AccountID)
 on update restrict on delete cascade
 );
 
 drop table if exists song;
 create table song (
-SongID varchar(20) not null,
-Album_Name varchar(30),
+SongID varchar(50) not null,
+Album_Name varchar(50),
 Album_CreatorID char(6),
 foreign key(SongID) references content(ContentID)
 on update restrict on delete cascade,
@@ -168,8 +171,8 @@ on update restrict on delete set null
 
 drop table if exists genres;
 create table genres (
-SongID varchar(20) not null,
-Genre_Name varchar(30) not null,
+SongID varchar(50) not null,
+Genre_Name varchar(50) not null,
 foreign key(SongID) references song(SongID)
 on update restrict on delete cascade
 );
@@ -177,8 +180,8 @@ on update restrict on delete cascade
 drop table if exists playlist;
 create table playlist (
 PlaylistID char(5) not null,
-Name varchar(30) not null,
-ListenerID char(6) not null,
+Name varchar(50) not null,
+ListenerID char(6),
 primary key(PlaylistID),
 foreign key(ListenerID) references listener(AccountID)
 on update restrict on delete cascade
@@ -187,7 +190,7 @@ on update restrict on delete cascade
 drop table if exists makes_up;
 create table makes_up (
 PlaylistID char(5) not null,
-SongID varchar(20) not null,
+SongID varchar(50) not null,
 Track_Order int not null,
 foreign key(PlaylistID) references playlist(PlaylistID)
 on update restrict on delete cascade,
